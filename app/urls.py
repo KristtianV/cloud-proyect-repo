5bbbe5d24ae1589d232e20600c1d2ca3919b196c
Cristian Vega <cristianvega.m@outlook.com>
2021-10-13 23:58:27 -0500

    Paso del proyecto a Django Creacion de los view, templates y urls de index, logout, login, startup y editprofile

--- app/urls.py
+++ app/urls.py
@@ -3,12 +3,24 @@ from .import views
 
 app_name = 'app'
 urlpatterns = [
-    path('',views.index, name='index'),
-    #lista de categorias
-    path('categorias/', views.categorias_view, name='categorias'),
-    #Ver una sola categoria
-    path('categorias/<int:id>', views.categoria_view, name='categoria'),
-    path('peliculas/', views.peliculas_view, name='peliculas'),
-    path('acerca/', views.acerca_view, name='acerca'),
+    #iniciar sesion
+    path('login/',views.login_view, name='login_view'),
+    path('login_post/',views.login_post, name='login_post'),
+    #Cerrar sesion
+    path('logout/',views.logout_, name='logout_'),
+    
+    #Crear usuario
+    path('startup/',views.startup_view, name='startup_view'),
+    path('startup_post/',views.startup_post, name='startup_post'),
+
+    #index
+    path('',views.index_view, name='index'),
+
+    #editar usuario
+    path('editProfile',views.editProfile_view, name='editProfile_view'),
+    path('editProfile_post',views.editProfile_post, name='editProfile_post'),
+
+
+
 ]
 
--- app/views.py
+++ app/views.py
@@ -1,28 +1,85 @@
-from django.http.response import HttpResponse
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http.response import  HttpResponse
+from django.shortcuts import redirect, render
+from django.http import HttpResponse, request
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
-def index(request):
-    return HttpResponse('¡¡¡HOLA MUNDO!!!')
+# Iniciar sesion
+def login_view(request):
+    return render(request,'app/login.html')
+    
+def login_post(request):
+    #obtener los datos del formulario
+    username = request.POST['username']
+    password = request.POST['password']
 
-def categorias_view(request):
-    return render(request, 'app/categorias.html')
+    #obtener el usuario
+    user = authenticate(username=username,password=password)
 
-def categoria_view(request, id):
-    contexto={
-        'actor':'jack Nicolson',
-        'esJubilado':True,
-        'filmografia':[
-            {'titulo':'naufrago','year':2021},
-            {'titulo':'otra','year':2020},
-            {'titulo':'poulfiction','year':2022},
-        ]
-    }
-    return render(request,'app/categoria.html',contexto)
+    if user is None:
+        return HttpResponse("Error: el usuario no existe")
+    else:
+        #inicia sesion
+        login(request, user)
+        return redirect('app:index')
 
-def peliculas_view(request):
-    return render(request,'app/peliculas.html')
+#Crear usuario
+def startup_view(request):
+    return render(request,'app/startup.html')
 
-def acerca_view(request):
-    return render(request,'app/acerca.html')
+def startup_post(request):
+    #obtiene datos del formulario"
+    username = request.POST['username']
+    password = request.POST['password']
+    email = request.POST['email']
+    name = request.POST['name']
+    lastname = request.POST['lastname']
+    # Verifica si existe
+    if User.objects.filter(username=username).exists():
+        return HttpResponse("Error:El nombre de usuario ya existe")
+    elif User.objects.filter(email=email).exists():
+        return HttpResponse("Error:El correo ya existe")
+    else:  
+        #crea el usuario
+        user=User()
+        user.username = username
+        user.set_password(password)
+        user.email = email
+        user.first_name = name
+        user.last_name = lastname
+        user.save()
+        return redirect('app:login_view')
+
+#editar perfil
+@login_required
+def editProfile_view(request):
+    return render(request,'app/editProfile.html')
+def editProfile_post(request):
+    #obtiene datos del formulario"
+    username = request.POST['username']
+    password = request.POST['password']
+    email = request.POST['email']
+    name = request.POST['name']
+    lastname = request.POST['lastname']
+    # Verifica si existe
+    if User.objects.filter(username=username).exists():
+        return HttpResponse("Error:El nombre de usuario ya existe")
+    elif User.objects.filter(email=email).exists():
+        return HttpResponse("Error:El correo ya existe")
+    else:  
+        #editar el usuario
+        return HttpResponse("editado")
+
+#Index
+@login_required
+def index_view(request):
+    return render(request,'app/index.html')
+
+#logout
+def logout_(request):
+    #cierra la sesion activa
+    logout(request)
+    return redirect('app:login_view')
